7c81d8c201c0d12e85df9e943a95347cefd926af
Rithika Silva <rithikasilva@outlook.com>
2021-12-16 22:23:58 -0500

    Added code to check for time.

--- main.py
+++ main.py
@@ -6,41 +6,56 @@ import random
 from unsplash import get_image
 from resize import resize_image
 from twitterbot import tweet_image
-
-# Get an image
-get_image()
-
-# Operate on the image
-y = random.randint(1,6)
-if y == 1:
-    half_invert_pixel_sort("image_temp")
-elif y == 2:
-    half_invert_square_displace("image_temp")
-elif y== 3:
-    half_invert_rotate_square("image_temp")
-elif y == 4:
-    pixelate("image_temp")
-elif y == 5:
-    displace_squares("image_temp")
-else:
-    s_pixels("image_temp")
+import time
+import datetime
 
 
-# Resize image for twitter restrictions
-resize_image()
+# get current time
+current_time = datetime.datetime.now()
+hour = current_time.hour
+minute = current_time.minute
 
-# Gets the Current Number
-f = open("number.txt", "r")
-number = int(f.read())
-f.close()
+# Run at 2:00pm at every day
+if (hour == 14) and (minute == 0):
 
-# Tweet the image
-tweet_image(number)
+    # Get an image
+    get_image()
+
+    # Operate on the image
+    y = random.randint(1,6)
+    if y == 1:
+        half_invert_pixel_sort("image_temp")
+    elif y == 2:
+        half_invert_square_displace("image_temp")
+    elif y== 3:
+        half_invert_rotate_square("image_temp")
+    elif y == 4:
+        pixelate("image_temp")
+    elif y == 5:
+        displace_squares("image_temp")
+    else:
+        s_pixels("image_temp")
 
 
-# Increments number
-number = number + 1
-f = open("number.txt", "w")
-f.write(str(number))
-f.close()
+    # Resize image for twitter restrictions
+    resize_image()
+
+    # Gets the Current Number
+    f = open("number.txt", "r")
+    number = int(f.read())
+    f.close()
+
+    # Tweet the image
+    tweet_image(number)
+
+
+    # Increments number
+    number = number + 1
+    f = open("number.txt", "w")
+    f.write(str(number))
+    f.close()
+
+    # wait 60 seconds so that we don't have two runs
+    time.sleep(60)
+
 
