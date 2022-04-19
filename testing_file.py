4a00814607fe0455cdca6344645aa641bb6c6cda
Rithika Silva <53627672+rithikasilva@users.noreply.github.com>
2022-04-19 21:32:54 +0000

    Modified testing file for easier testing.

--- testing_file.py
+++ testing_file.py
@@ -1,24 +1,55 @@
-from imagedisplacement import square_rotate, displace_squares, half_back, invert_half
-from pixelate import pixelate_image, pixelate_piece
-from psorting import s_pixels
+import imagedisplacement
+import pixelate
+import psorting
 import random
-from unsplash import get_image
-from resize import resize_image
-from twitterbot import tweet_image
+import unsplash
+import resize
+import twitterbot
 import swirl
 
 
+# Image name
+imagename = "image_temp"
+
+# Get an image
+unsplash.get_image(imagename)
+
+# Resize image for twitter restrictions and easier computation
+resize.resize_image(imagename)
 
 
-get_image()
+
+y = input("Select an option:")
+y = int(y)
+
+if y == 1:
+  imagedisplacement.invert_half(imagename)
+  psorting.s_pixels(imagename)
+elif y == 2:	
+  imagedisplacement.invert_half(imagename)
+  imagedisplacement. displace_squares(imagename)
+elif y == 3:	
+  imagedisplacement.invert_half(imagename)
+  imagedisplacement.square_rotate(imagename)
+elif y == 4:
+	pixelate.pixelate_image(imagename)
+elif y == 5:
+	imagedisplacement.displace_squares(imagename)
+elif y == 6 or y == 7 or y == 8: 
+	psorting.s_pixels(imagename)
+elif y == 9:
+  psorting.s_pixels(imagename)
+  swirl.swirl_image(-360, imagename)
+elif y == 10:
+  swirl.swirl_image(-360, imagename)
+else:
+  imagedisplacement.invert_colour(imagename)
+  
 
 
-resize_image()
 
 
 
-#Write Algorithm to Test Here
-swirl.swirl_image(-360, "image_temp")
-
-
+# Feedback for completion
 print("done")
+
